b6eeb8f53a98b2bfcad89da8eb36350e5dd4a9f4
IanMcT <ian.mctavish@tldsb.on.ca>
2018-05-15 14:45:05 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace snakeGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int counter = 0;
+        System.Windows.Threading.DispatcherTimer gameTimer = new System.Windows.Threading.DispatcherTimer();
+        Snake snake;
+        public MainWindow()
+        {
+            InitializeComponent();
+            snake = new Snake(canvas, this);
+            //start Timer
+            gameTimer.Tick += GameTimer_Tick;
+            gameTimer.Interval = new TimeSpan(0, 0, 0, 1, 0);//fps
+            gameTimer.Start();
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            counter++;
+            if (counter % 3 == 0)
+            {
+                snake.grow();
+                counter = 0;
+            }
+            else
+            {
+                snake.update();
+            }
+            
+        }
+    }
+}
---
+++ Snake.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace snakeGame
+{
+    enum Direction {up, right, down, left }
+    class Snake
+    {
+        //Variables
+        Direction direction;
+        int speed;
+        List<Rectangle> body;
+        Canvas canvas;
+        Window window;
+
+        //Constructor
+        public Snake(Canvas c, Window w)
+        {
+            canvas = c;
+            window = w;
+            body = new List<Rectangle>();
+            Rectangle temp = new Rectangle();
+            temp.Fill = Brushes.Red;
+            temp.Width = 10;
+            temp.Height = 10;
+            body.Add(temp);
+            for (int i = 0; i < body.Count; i++)
+            {
+                canvas.Children.Add(body[i]);
+                Canvas.SetTop(body[i], 0);
+                Canvas.SetLeft(body[i], 0);
+            }
+            direction = Direction.right;
+
+        }
+
+        public void update()
+        {
+            if (direction == Direction.right)
+            {
+                Rectangle temp = new Rectangle();
+                temp.Fill = Brushes.Red;
+                temp.Width = 10;
+                temp.Height = 10;
+                body.Add(temp);
+                canvas.Children.Add(body[body.Count - 1]);
+                double x = Canvas.GetLeft(body[body.Count - 2]);
+                Canvas.SetTop(body[body.Count - 1], Canvas.GetTop(body[body.Count - 2]));
+                Canvas.SetLeft(body[body.Count - 1], Canvas.GetLeft(body[body.Count - 2]) + 10);
+                foreach (Rectangle r in canvas.Children) {
+                    //Console.WriteLine(Canvas.get)
+                }
+                Console.WriteLine(canvas.Children.Count);
+                canvas.Children.Remove(body[0]);
+                body.RemoveAt(0);
+            }
+            
+        }
+
+        internal void grow()
+        {
+            if (direction == Direction.right)
+            {
+                Rectangle temp = new Rectangle();
+                temp.Fill = Brushes.Red;
+                temp.Width = 10;
+                temp.Height = 10;
+                body.Add(temp);
+                canvas.Children.Add(body[body.Count - 1]);
+                Canvas.SetTop(body[body.Count - 1], Canvas.GetTop(body[body.Count - 2]));
+                Canvas.SetLeft(body[body.Count - 1], Canvas.GetLeft(body[body.Count - 2]) + 10);
+            }
+        }
+        }
+    }
